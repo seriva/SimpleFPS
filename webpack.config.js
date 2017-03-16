76181bc31c22853bd8b2277f5ea0027c1f28b1ba
Luuk van Venrooij <luuk.van-venrooij@pl.abb.com>
2017-03-16 16:55:58 +0100

    Cleaned up webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -18,23 +18,5 @@ module.exports = {
             loader: 'babel-loader?presets[]=es2017'
             }
         ]
-    },
-    devServer: {
-        stats: {
-            colors: true,
-            hash: true,
-            version: true,
-            timings: true,
-            assets: true,
-            chunks: true,
-            modules: true,
-            reasons: true,
-            children: true,
-            source: true,
-            errors: true,
-            errorDetails: true,
-            warnings: true,
-            publicPath: true
-        }
     }
 };
