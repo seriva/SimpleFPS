bd1e063b27e8c860dbfc96b78319f6b5b401aabc
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2017-06-20 11:13:03 +0200

    Removed some unused stuff

--- webpack.config.js
+++ webpack.config.js
@@ -13,16 +13,10 @@ module.exports = {
     module: {
         rules: [
             {
-                enforce: "pre",
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "eslint-loader",
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-            },
+                loader: ["babel-loader", "eslint-loader"]
+            }
         ]
     }
 };
