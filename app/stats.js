59807430aaba001b969bc91ebc20fa01eaa6ae71
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2017-09-20 08:30:00 +0200

    Simplefied stats.

--- app/stats.js
+++ app/stats.js
@@ -1,6 +1,6 @@
 import Utils from './utils';
 
-let showStats = true;
+let visible = true;
 let fps = 0;
 let fpscounter = 0;
 let frametime = 0;
@@ -8,54 +8,45 @@ let frametime = 0;
 // add css for stats
 Utils.addCSS(
     `
-    #stat-fps { 
-        left: 15px; 
-        top: 15px; 
-        margin: 0; 
-        padding: 0; 
-        position: absolute; 
+    #stats {
+        left: 0px;
+        top: 0px;
+        margin: 0;
+        padding: 0;
         color: #FFF;
         z-index: 150;
         font-size: 14px;
     }
 
-    #stat-ftm { 
-        left: 15px; 
-        top: 30px; 
-        margin: 0; 
-        padding: 0; 
-        position: absolute; 
-        color: #FFF;
-        z-index: 150;
-        font-size: 14px;
+    #stats-fps { 
+        left: 15px;
+        top: 15px;
+        position: absolute;
+    }
+
+    #stats-ftm {
+        left: 15px;
+        top: 30px;
+        position: absolute;
     }
     `
 );
 
-const fpsSpan = Utils.addElement('span', 'stat-fps');
-const ftmSpan = Utils.addElement('span', 'stat-ftm');
+const statsDiv = Utils.addElement('div', 'stats');
+const fpsSpan = Utils.addElement('span', 'stats-fps', statsDiv);
+const ftmSpan = Utils.addElement('span', 'stats-ftm', statsDiv);
 
 window.setInterval(() => {
     fps = fpscounter;
     fpscounter = 0;
-    document.getElementById('stat-fps').innerHTML = 'FPS : ' + fps.toPrecision(5);
-    document.getElementById('stat-ftm').innerHTML = 'FTM : ' + frametime.toPrecision(5);
+    fpsSpan.innerHTML = 'FPS : ' + fps.toPrecision(5);
+    ftmSpan.innerHTML = 'FTM : ' + frametime.toPrecision(5);
 }, 1000);
 
 const Stats = {
     toggle(show) {
-        if (show === undefined) {
-            showStats = !showStats;
-        } else {
-            showStats = show;
-        }
-        if (showStats) {
-            fpsSpan.style.display = 'block';
-            ftmSpan.style.display = 'block';
-        } else {
-            fpsSpan.style.display = 'none';
-            ftmSpan.style.display = 'none';
-        }
+        show === undefined ? visible = !visible : visible = show;
+        visible ? statsDiv.style.display = 'block' : statsDiv.style.display = 'none';
     },
 
     update(ft) {
