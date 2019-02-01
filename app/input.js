9b1a8001b5f6c5859aca77c063cbd00e1738b4ba
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2019-02-01 18:23:03 +0100

    Refactored touch input.

--- app/input.js
+++ app/input.js
@@ -1,5 +1,3 @@
-import Hammer from 'hammerjs';
-import nipplejs from 'nipplejs';
 import Utils from './utils';
 import Settings from './settings';
 import DOM from './dom';
@@ -9,6 +7,16 @@ const h = DOM.h;
 DOM.registerCSS({
     '#input': {
         zIndex: 500
+    },
+
+    '#look': {
+        width: '50%',
+        height: '100%',
+        right: '0px',
+        bottom: '0px',
+        margin: 0,
+        padding: 0,
+        position: 'absolute'
     },
 
     '#cursor': {
@@ -24,29 +32,45 @@ DOM.registerCSS({
     },
 
     '#move': {
-        width: '200px',
+        width: '50%',
         height: '100%',
         left: '0px',
         bottom: '0px',
         margin: 0,
-        padding: 0
+        padding: 0,
+        position: 'absolute'
     },
 
-    '#look': {
-        width: 'calc(100% - 200px)',
-        height: '100%',
-        right: '0px',
-        bottom: '0px',
-        margin: 0,
-        padding: 0,
+    '#joystick': {
+        background: 'white',
+        width: '100px',
+        height: '100px',
+        left: '35px',
+        bottom: '35px',
         position: 'absolute',
-        opacity: 0.01
+        opacity: 0.5,
+        borderRadius: '50%',
+        zIndex: 501
+    },
+
+    '#stick': {
+        background: 'white',
+        borderRadius: '100%',
+        cursor: 'pointer',
+        userSelect: 'none',
+        width: '50px',
+        height: '50px',
+        left: '60px',
+        bottom: '60px',
+        position: 'absolute',
+        opacity: 0.5,
+        zIndex: 502
     }
 });
 
 let visibleCursor = true;
 let virtualInputVisible = Utils.isMobile();
-let cursorMovement = {
+const cursorMovement = {
     x: 0,
     y: 0
 };
@@ -58,10 +82,10 @@ let input = null;
 
 window.addEventListener(
     'keyup',
-    (event) => {
-        delete pressed[event.keyCode];
+    (ev) => {
+        delete pressed[ev.keyCode];
         for (let l = 0; l < upevents.length; l++) {
-            if (upevents[l].key === event.keyCode) {
+            if (upevents[l].key === ev.keyCode) {
                 upevents[l].event();
             }
         }
@@ -76,10 +100,10 @@ window.addEventListener(
 
 window.addEventListener(
     'keydown',
-    (event) => {
-        pressed[event.keyCode] = true;
+    (ev) => {
+        pressed[ev.keyCode] = true;
         for (let l = 0; l < downevents.length; l++) {
-            if (downevents[l].key === event.keyCode && !downevents[l].pressed) {
+            if (downevents[l].key === ev.keyCode && !downevents[l].pressed) {
                 downevents[l].event();
                 downevents[l].pressed = true;
             }
@@ -90,31 +114,186 @@ window.addEventListener(
 
 window.addEventListener(
     'mousemove',
-    (evt) => {
-        cursorMovement = {
-            x: evt.movementX,
-            y: evt.movementY
-        };
+    (ev) => {
+        cursorMovement.x = ev.movementX;
+        cursorMovement.y = ev.movementY;
         if (timeout !== undefined) {
             window.clearTimeout(timeout);
         }
         timeout = window.setTimeout(() => {
-            cursorMovement = {
-                x: 0,
-                y: 0
-            };
+            cursorMovement.x = 0;
+            cursorMovement.y = 0;
         }, 50);
     },
     false
 );
 
 if (Utils.isMobile()) {
-    const move = h('div#move');
     const look = h('div#look');
     const cursor = h('div#cursor');
+    const stick = h('div#stick');
+    const joystick = h('div#joystick');
+    const move = h('div#move', [joystick, stick]);
     input = h('div#input', [move, look, cursor]);
     DOM.append(() => input);
 
+    let cursorPos = null;
+
+    // touch cursor/mouse
+    look.domNode.addEventListener(
+        'touchstart',
+        (ev) => {
+            if (ev.targetTouches) {
+                cursorPos = {
+                    x: ev.targetTouches[0].clientX,
+                    y: ev.targetTouches[0].clientY
+                };
+            }
+            DOM.animate(
+                cursor.domNode,
+                { opacity: 0.5 },
+                {
+                    mobileHA: false,
+                    duration: 100,
+                    delay: 0,
+                    easing: 'ease-in'
+                }
+            );
+        },
+        false
+    );
+    look.domNode.addEventListener(
+        'touchend',
+        () => {
+            cursorPos = null;
+            DOM.animate(
+                cursor.domNode,
+                { opacity: 0.0 },
+                {
+                    mobileHA: false,
+                    duration: 100,
+                    delay: 0,
+                    easing: 'ease-in'
+                }
+            );
+        },
+        false
+    );
+    look.domNode.addEventListener(
+        'touchmove',
+        (ev) => {
+            ev.preventDefault();
+            if (ev.targetTouches) {
+                cursorPos = {
+                    x: ev.targetTouches[0].clientX,
+                    y: ev.targetTouches[0].clientY
+                };
+            }
+        },
+        false
+    );
+
+    // touch joystick
+    let dragStart = null;
+    let stickPos = null;
+
+    stick.domNode.addEventListener('touchstart', (ev) => {
+        stick.domNode.style.transition = '0s';
+        if (ev.targetTouches) {
+            dragStart = {
+                x: ev.targetTouches[0].clientX,
+                y: ev.targetTouches[0].clientY
+            };
+            return;
+        }
+        dragStart = {
+            x: ev.clientX,
+            y: ev.clientY
+        };
+    });
+    move.domNode.addEventListener('touchend', () => {
+        if (dragStart === null) return;
+        stick.domNode.style.transition = '.2s';
+        stick.domNode.style.transform = 'translate3d(0px, 0px, 0px)';
+        delete pressed[Settings.forward];
+        delete pressed[Settings.backwards];
+        delete pressed[Settings.left];
+        delete pressed[Settings.right];
+        dragStart = null;
+        stickPos = null;
+    });
+    move.domNode.addEventListener('touchmove', (ev) => {
+        ev.preventDefault();
+        if (dragStart === null) return;
+        if (ev.targetTouches) {
+            ev.clientX = ev.targetTouches[0].clientX;
+            ev.clientY = ev.targetTouches[0].clientY;
+        }
+        const xDiff = ev.clientX - dragStart.x;
+        const yDiff = ev.clientY - dragStart.y;
+        const angle = Math.atan2(yDiff, xDiff);
+        const distance = Math.min(50, Math.hypot(xDiff, yDiff));
+        stickPos = {
+            x: distance * Math.cos(angle),
+            y: distance * Math.sin(angle)
+        };
+
+        let dAngle = angle * (180 / Math.PI);
+        if (dAngle < 0) {
+            dAngle = 360 - Math.abs(dAngle);
+        }
+
+        if (dAngle && distance > 20) {
+            delete pressed[Settings.forward];
+            delete pressed[Settings.backwards];
+            delete pressed[Settings.left];
+            delete pressed[Settings.right];
+            const a = dAngle;
+            if ((a >= 337.5 && a < 360) || (a >= 0 && a < 22.5)) {
+                pressed[Settings.right] = true;
+            }
+            if (a >= 22.5 && a < 67.5) {
+                pressed[Settings.right] = true;
+                pressed[Settings.backwards] = true;
+            }
+            if (a >= 67.5 && a < 112.5) {
+                pressed[Settings.backwards] = true;
+            }
+            if (a >= 112.5 && a < 157.5) {
+                pressed[Settings.backwards] = true;
+                pressed[Settings.left] = true;
+            }
+            if (a >= 157.5 && a < 202.5) {
+                pressed[Settings.left] = true;
+            }
+            if (a >= 202.5 && a < 247.5) {
+                pressed[Settings.left] = true;
+                pressed[Settings.forward] = true;
+            }
+            if (a >= 247.5 && a < 292.5) {
+                pressed[Settings.forward] = true;
+            }
+            if (a >= 292.5 && a < 337.5) {
+                pressed[Settings.forward] = true;
+                pressed[Settings.right] = true;
+            }
+        }
+    });
+
+    // update cursor/joystink
+    const updateInput = () => {
+        if (stickPos !== null) {
+            stick.domNode.style.transform = `translate3d(${stickPos.x}px, ${stickPos.y}px, 0px)`;
+        }
+        if (cursorPos !== null) {
+            cursor.domNode.style.left = `${cursorPos.x}px`;
+            cursor.domNode.style.top = `${cursorPos.y}px`;
+        }
+        window.requestAnimationFrame(updateInput);
+    };
+    window.requestAnimationFrame(updateInput);
+
+    /*
     const lookTouch = new Hammer(look.domNode, {
         touchAction: 'auto',
         inputClass: Hammer.TouchInput
@@ -135,83 +314,8 @@ if (Utils.isMobile()) {
                 cursor.domNode.style.top = `${ev.pointers[0].clientY}px`;
             }
         }
-        if (ev.type === 'panstart') {
-            DOM.animate(
-                cursor.domNode,
-                { opacity: 0.5 },
-                {
-                    mobileHA: false,
-                    duration: 100,
-                    delay: 0,
-                    easing: 'ease-in'
-                }
-            );
-        }
-        if (ev.type === 'panend') {
-            DOM.animate(
-                cursor.domNode,
-                { opacity: 0.0 },
-                {
-                    mobileHA: false,
-                    duration: 100,
-                    delay: 0,
-                    easing: 'ease-in'
-                }
-            );
-        }
     });
-
-    // WASD input with virtual joystick
-    const moveJoystick = nipplejs.create({
-        zone: move.domNode,
-        mode: 'static',
-        position: { left: '80px', bottom: '80px' },
-        color: 'white'
-    });
-    moveJoystick
-        .on('move', (evt, data) => {
-            if (data.angle && data.distance && data.distance > 20) {
-                delete pressed[Settings.forward];
-                delete pressed[Settings.backwards];
-                delete pressed[Settings.left];
-                delete pressed[Settings.right];
-                const a = data.angle.degree;
-                if ((a >= 337.5 && a < 360) || (a >= 0 && a < 22.5)) {
-                    pressed[Settings.right] = true;
-                }
-                if (a >= 22.5 && a < 67.5) {
-                    pressed[Settings.right] = true;
-                    pressed[Settings.forward] = true;
-                }
-                if (a >= 67.5 && a < 112.5) {
-                    pressed[Settings.forward] = true;
-                }
-                if (a >= 112.5 && a < 157.5) {
-                    pressed[Settings.forward] = true;
-                    pressed[Settings.left] = true;
-                }
-                if (a >= 157.5 && a < 202.5) {
-                    pressed[Settings.left] = true;
-                }
-                if (a >= 202.5 && a < 247.5) {
-                    pressed[Settings.left] = true;
-                    pressed[Settings.backwards] = true;
-                }
-                if (a >= 247.5 && a < 292.5) {
-                    pressed[Settings.backwards] = true;
-                }
-                if (a >= 292.5 && a < 337.5) {
-                    pressed[Settings.backwards] = true;
-                    pressed[Settings.right] = true;
-                }
-            }
-        })
-        .on('end', () => {
-            delete pressed[Settings.forward];
-            delete pressed[Settings.backwards];
-            delete pressed[Settings.left];
-            delete pressed[Settings.right];
-        });
+    */
 }
 
 const Input = {
@@ -248,20 +352,16 @@ const Input = {
 
     addKeyDownEvent(key, event) {
         downevents.push({
-            /* eslint-disable */
-            key: key,
-            event: event,
-            /* eslint-disable */
+            key,
+            event,
             pressed: false
         });
     },
 
     addKeyUpEvent(key, event) {
         upevents.push({
-            /* eslint-disable */
-            key: key,
-            event: event
-            /* eslint-disable */
+            key,
+            event
         });
     },
 
